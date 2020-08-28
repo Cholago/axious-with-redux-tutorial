4371287a9f80a7aa800176a4b6a0f54e0994404c
Cholago <cholawilson@yahoo.com>
2020-08-28 12:57:53 +0300

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## AXIOS AND REACT TUTORIAL
+Allow this on your server if your using php `header("Access-Control-Allow-Origin: *");`
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import axios from "axios"
 
 
 const getUsers = () => {
-  axios.get(`http://192.168.43.62/mdko/index.php/test`).then()
-  console.log('Get users buton clicked');
 
+  axios.get(`http://192.168.43.62/mdko/index.php/test`,)
+    .then(res => {
+      console.log('Recieved data', res);
+      //console.log('Actual data data', res.data)
+    })
+    .catch(error => console.log(error));
 }
 
 function App() {
